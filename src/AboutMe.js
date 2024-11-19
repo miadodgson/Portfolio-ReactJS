43b8a99b6cc64ba98e54b8276cea7bd017a0086b
miadodgson <mddodgson@mun.ca>
2024-11-19 00:42:29 +0000

    Added description to about me section

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -10,6 +10,9 @@ return (
 		<h1 className="about-me"> About Me
 		</h1>
 		<hr />
+		<h2 className='h2' >A biochemist with a passion for molecular biology, molecular medicine, sustainable resource use and
+data-driven decision making.
+	</h2>
 	</div>
 	)
 }
