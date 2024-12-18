fe07ca15ba209e12c2a857cd380cd501be453947
miadodgson <mddodgson@mun.ca>
2024-12-18 20:06:10 +0000

    Added Skill Cards to Experience section

---
+++ src/Routes/Experience/Experience.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Experience.css'
+import SkillCard from '../../Components/SkillCard/SkillCard.js'
+
+const Experience = () => {
+
+
+return (
+	<div>
+		<h1 className='h1'> Experience </h1>
+		<SkillCard />
+	</div>
+	)
+}
+
+
+export default Experience
