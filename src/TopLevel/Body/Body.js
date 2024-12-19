547f93cc8e8ca0548da060db3f5a637dc4864460
miadodgson <mariadodgson99@.com>
2024-12-19 20:26:59 -0330

    Adjusted the Body element so that it spans the entire page

--- src/TopLevel/Body/Body.js
+++ src/TopLevel/Body/Body.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import '../App/App.css'
+import './Body.css'
 import Contact from '../../Routes/Contact/Contact.js'
 import LandingPage from '../../Routes/LandingPage/LandingPage.js'
 import Education from '../../Routes/Education/Education.js'
@@ -9,7 +10,7 @@ import Publications from '../../Routes/Publications/Publications.js'
 
 const Body = () => {
     return (
-        <div>
+        <div className='body-content-container'>
             <Routes>
                 <Route path='/' element={<LandingPage />}/>
 				<Route path='/Education' element={<Education />}/>
