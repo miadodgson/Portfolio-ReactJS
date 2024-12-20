a77a864156d39f1ead33e5b8c8a04ff041b91be7
miadodgson <mariadodgson99@.com>
2024-12-19 22:40:20 -0330

    Remove unused Image component and styles; update layout and styles for improved design

--- src/Routes/LandingPage/LeftContent.js
+++ src/Routes/LandingPage/LeftContent.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import '../../TopLevel/App/App.css'
-import Image from '../../Assets/Images/Image.js'
-import '../../Assets/Images/Image.css'
 import CustomButton from '../../CustomButton'
+import './LandingPage.css'
+import img from '../../Assets/Images/MyImage.png'
 
 const LeftContent = () => {
 
@@ -12,8 +12,8 @@ const handleClick = () => {
 
 return (
 	<div className="left-content">
-		<Image />
-		<h1 className="title" > Biochemistry Researcher
+		<img src={img} alt="image" className="profile-image" />
+		<h1 className="job-title" > Biochemistry Researcher
 		</h1>
 		<CustomButton label="Email Me" onClick={handleClick} />
 	</div>
