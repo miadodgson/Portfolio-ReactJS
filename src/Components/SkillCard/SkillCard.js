e53f36137e84401aa6238c0d7033ae022e73edd2
miadodgson <mariadodgson99@hotmail.com>
2024-12-23 22:26:47 +0000

    Updated skill cards to have module.css styling

--- src/Components/SkillCard/SkillCard.js
+++ src/Components/SkillCard/SkillCard.js
@@ -1,32 +1,32 @@
 import React from 'react';
-import './SkillCard.css';
+import styles from './SkillCard.module.css';
 
 const SkillCard = () => {
   return (
     /* From Uiverse.io by naveenkumarr-onyx */
-    <div className="card">
-      <div className="header">My Skills</div>
-      <div className="body">
-        <div className="skill">
-          <div className="skill-name html">Cell culture</div>
-          <div className="skill-level">
-            <div style={{ width: '90%' }} className="skill-percent"></div>
+    <div className={styles.card}>
+      <div className={styles.header}>My Skills</div>
+      <div className={styles.body}>
+        <div className={styles.skill}>
+          <div className={styles.skillName }>Cell culture</div>
+          <div className={styles.skillLevel}>
+            <div style={{ width: '90%' }} className={styles.skillPercent}></div>
           </div>
-          <div className="skill-percent-number">90%</div>
+          <div className={styles.skillPercentLevel}>90%</div>
         </div>
-        <div className="skill">
-          <div className="skill-name css">Western Blot</div>
-          <div className="skill-level">
-            <div style={{ width: '80%' }} className="skill-percent"></div>
+        <div className={styles.skill}>
+          <div className={styles.skillName}>Western Blot</div>
+          <div className={styles.skillLevel}>
+            <div style={{ width: '80%' }} className={styles.skillPercent}></div>
           </div>
-          <div className="skill-percent-number">80%</div>
+          <div className={styles.skillPercentNumber}>80%</div>
         </div>
-        <div className="skill">
-          <div className="skill-name js">Quantitative PCR</div>
-          <div className="skill-level">
-            <div style={{ width: '75%' }} className="skill-percent"></div>
+        <div className={styles.skills}>
+          <div className={styles.skillName}>Quantitative PCR</div>
+          <div className={styles.skillLevel}>
+            <div style={{ width: '75%' }} className={styles.skillPercent}></div>
           </div>
-          <div className="skill-percent-number">75%</div>
+          <div className={styles.skillPercentNumber}>75%</div>
         </div>
       </div>
     </div>
