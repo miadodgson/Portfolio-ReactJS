a50ddff48aa30e918f5784e42cb7c81f2ed4dcb7
miadodgson <mariadodgson99@hotmail.com>
2024-12-31 03:28:25 +0000

    Updated header, navbar, and contact card icons and color

--- src/TopLevel/Header/Header.js
+++ src/TopLevel/Header/Header.js
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom'
 import styles from './Header.module.css'
 import Nav from '../Navbar/Navbar'
+import Flask from '../../Assets/Images/Flask.png'
 
 const Header = () => {
 	const navigate = useNavigate()
@@ -8,7 +9,9 @@ const Header = () => {
 	return (
 		<div>
 			<div className={styles.header}>
-						<div className={styles.loader} onClick={() => navigate('/')} ></div>				
+						<div className={styles.loader} onClick={() => navigate('/')} >
+							<img src={Flask} className={styles.loader}alt="Flask"/>
+						</div>				
 				<Nav/>
 			</div>
 		</div>
