26607426e3f5c585842bdec86008564fb58b9cc7
miadodgson <mariadodgson99@.com>
2025-03-24 17:35:02 -0230

    Commented out missing image file usage and imports.

--- src/Routes/Resources/Resources.js
+++ src/Routes/Resources/Resources.js
@@ -1,6 +1,6 @@
 import styles from './Resources.module.css';
-import ReactIcon from '../../Assets/Images/ReactIcon2.svg'
-import FreepikIcon from '../../Assets/Images/FreepikIcon.png'
+// import ReactIcon from '../../Assets/Images/ReactIcon2.svg'
+// import FreepikIcon from '../../Assets/Images/FreepikIcon.png'
 
 
 const handleClick = () => {
@@ -11,11 +11,11 @@ const Resources = () => {
 
   return (
 	    <div className={styles.container} >
-			<img src={ReactIcon} className={styles.reactIcon} alt="React Icon"/>
+			{/* <img src={ReactIcon} className={styles.reactIcon} alt="React Icon"/> */}
 			<button className={styles.button} onClick={handleClick}>
 			      <div className={styles.ui}>UI</div>VERSE
 		    </button>
-			<img src={FreepikIcon} className={styles.freepikIcon} alt="Freekpik Icon"/>
+			{/* <img src={FreepikIcon} className={styles.freepikIcon} alt="Freekpik Icon"/> */}
 			<a href="https://www.flaticon.com/free-icons/navigation" title="navigation icons">Navigation icons created by Freepik - Flaticon</a>
 		</div>
 	)
